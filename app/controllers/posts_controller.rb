51d59572db392245a540f0da38eb0eadd7fdee46
behter <behter198510@rambler.ru>
2014-02-05 18:54:19 +0200

    Create model Post, view's, and controlller posts_controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+
+	def new
+		@post = Post.new
+  	end
+
+	def index # show all public's
+  		@posts = Post.all
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+	def post_params
+    	params.require(:post).permit(:title, :text)
+  	end
+
+  	def show # show current publication
+  		@post = Post.find(params[:id])
+  	end
+
+  	def edit
+  		@post = Post.find(params[:id])
+	end
+	
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+  		@post = Post.find(params[:id])
+  		@post.destroy 
+  		redirect_to posts_path
+	end
+
+end
